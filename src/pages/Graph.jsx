15c908c2846fb9b59b117274e5500357cdc129d9
hnrqblck <hnrqblck@gmail.com>
2022-05-26 11:05:53 -0300

    [JS-207] feat: add csv & json export buttons and files

--- src/pages/Graph.jsx
+++ src/pages/Graph.jsx
@@ -10,8 +10,20 @@ import {
   Spinner,
   Flex,
   Heading,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
 } from "@chakra-ui/react";
+import { ChevronDownIcon } from '@chakra-ui/icons'
+
 import * as api from "strateegia-api";
+import { CSVLink } from 'react-csv';
 
 export default function Graph() {
   const graphRef = useRef();
@@ -22,6 +34,13 @@ export default function Graph() {
   const [selectedProject, setSelectedProject] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [projectList, setProjectList] = useState([]);
+  const [nodesCsv, setNodesCsv] = useState([]);
+  const [linksCsv, setLinksCsv] = useState([]);
+
+
+  function handleSelectChange(e) {
+    setSelectedProject(e.target.value);
+  }
 
   function handleSelectChange(e) {
     setSelectedProject(e.target.value);
@@ -34,7 +53,7 @@ export default function Graph() {
         const accessToken = localStorage.getItem("accessToken");
         setIsLoading(true);
         const graphData = await gatherGraphData(accessToken, selectedProject);
-        // setGraphData(graphData);
+        setGraphData(graphData);
         setNodes([...graphData.nodes]);
         setLinks([...graphData.links]);
         setIsLoading(false);
@@ -52,6 +71,55 @@ export default function Graph() {
   }, [selectedProject]);
 
   useEffect(() => {
+    // groups: ["project", "map", "divpoint", "question", "comment", "reply", "agreement", "user", "users"]
+
+    const getNodesData = (groupName) => graphData.nodes?.filter(({group}) => group === groupName);
+    const getLinksData = () => graphData.links?.map((data) => data);
+
+    function createNodesObj (arr) {
+      return arr?.map((data) => {
+        return {
+          id: data.id,
+          group: data.group,
+          title: data.title,
+          creation: data.createdAt,
+          url: data.dashboardUrl,
+        }
+      });
+    }
+
+    function createLinksObj (arr) {
+      return arr?.map((data) => {
+        return {
+          target: data.target,
+          source: data.source,
+        }
+      });
+    }
+
+    if (selectedProject) {
+
+      const maps = createNodesObj(getNodesData('map'));
+      const divPoints = createNodesObj(getNodesData('divpoint'));
+      const questions = createNodesObj(getNodesData('question'));
+      const comments = createNodesObj(getNodesData('comment'));
+      const replies = createNodesObj(getNodesData('reply'));
+      const agreements = createNodesObj(getNodesData('agreement'));
+
+      const links = createLinksObj(getLinksData());
+
+      setNodesCsv(() => [...maps, ...divPoints, ...questions, ...comments, ...replies, ...agreements]);
+      setLinksCsv(() => [...links]);
+    }
+    // console.log
+  }, [graphData]);
+
+  useEffect(() => {
+    console.log(nodesCsv);
+    console.log(linksCsv);
+  }, [nodesCsv, links])
+
+  useEffect(() => {
     async function fetchProjectList() {
       try {
         const accessToken = localStorage.getItem("accessToken");
@@ -66,12 +134,7 @@ export default function Graph() {
   }, []);
 
   return (
-    // <div>
-    //   <h1>Debug Graph</h1>
-    //   <p>{nodes.length}</p>
-    //   <p>{links.length}</p>
-    //   <pre>{JSON.stringify(graphData, null, 2)}</pre>
-    // </div>
+   
     <Box>
       <Heading as="h3" size="md" p={3}>
         metaverso da jornada
@@ -90,7 +153,8 @@ export default function Graph() {
       {/* <Input type="text" placeholder={selectedProject} />
       <Input type="text" placeholder={isLoading ? 'LOADING' : 'NOT LOADING'} /> */}
       {selectedProject === "" ? (
-        <></>
+        <>
+        </>
       ) : isLoading ? (
         <Flex minH={"100vh"} align={"center"} justify={"center"}>
           <Stack>
@@ -98,6 +162,76 @@ export default function Graph() {
           </Stack>
         </Flex>
       ) : (
+        <>
+        <Box display='flex' justifyContent='flex-end' m='4px'>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} size='xs'
+              fontSize='14px'
+              fontWeight='400'
+              bg='#6c757d' 
+              color='#fff'
+              borderRadius='3px'
+              _hover={{bg: '#5C636A'}}
+              paddingBottom={'4px'}
+              marginRight='4px'  
+            >
+              csv
+            </MenuButton>
+            <MenuList 
+              size='sm'
+              bg='#6c757d' 
+              color='#fff'
+              minW='40px'
+              fontSize='14px'
+            >
+              <CSVLink data={nodesCsv} filename='strateegia_metaverse_nodes_report-csv.csv' >
+                <MenuItem
+                  _hover={{bg: '#5C636A'}}
+                  _focus={{bg: '#5C636A'}}
+                >
+                  nodes.csv
+                </MenuItem>
+              </CSVLink>
+              <CSVLink data={linksCsv} filename='strateegia_metaverse_links_report-csv.csv' >
+                <MenuItem _hover={{bg: '#5C636A'}}>links.csv</MenuItem>
+              </CSVLink>
+            </MenuList>
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} size='xs'
+                fontSize='14px'
+                fontWeight='400'
+                bg='#6c757d' 
+                color='#fff'
+                borderRadius='3px'
+                _hover={{bg: '#5C636A'}}
+                paddingBottom={'4px'}>
+              json
+            </MenuButton>
+            <MenuList 
+              size='sm'
+              bg='#6c757d' 
+              color='#fff'
+              minW='40px'
+              fontSize='14px'
+            >
+                <MenuItem 
+                  _hover={{bg: '#5C636A'}}
+                  _focus={{bg: '#5C636A'}}
+                  onClick={() => exportJSONData(nodesCsv, 'nodes')}
+                >
+                  nodes.json
+                </MenuItem>
+                <MenuItem 
+                  _hover={{bg: '#5C636A'}}
+                  onClick={() => exportJSONData(linksCsv, 'links')}
+                >
+                  links.json
+                </MenuItem>
+            </MenuList>
+          </Menu>
+          
+        </Box>
         <ForceGraph3D
           ref={graphRef}
           graphData={{ nodes, links }}
@@ -112,7 +246,20 @@ export default function Graph() {
           // dagMode={'radialout'}
           // onEngineStop={() => graphRef.current.zoomToFit(400)}
         />
+        </>
       )}
     </Box>
   );
+}
+
+export const exportJSONData = (data, type) => {
+  const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+    JSON.stringify(data)
+  )}`;
+
+  const link = document.createElement("a");
+  link.href = jsonString;
+  link.download = `strateegia_metaverse_${type}_report-json.json`;
+
+  link.click();
 }
